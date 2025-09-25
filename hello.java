49b9e4012c02bc28338b5cc99bb36b8e76afc8e8
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-09-25 20:18:13 +0700

    learn data type

--- hello.java
+++ hello.java
@@ -1,5 +1,18 @@
 class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int number = 10;
+        number = 20;
+        System.out.println (number);
+
+        int num1 = 10;
+        byte by = 123;
+        short sh = 12345;
+        long lo = 1234567890;
+        float fl = 123.45f;
+        double db = 123.4567890;
+        char ch = 'A';
+        boolean bo = true;
+        String str = "Hello, World!";
     }
 }
