190e94d72f1f9fcc172ff7298e62fbcc8b9fdd5f
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-09-27 18:39:48 +0700

    im coming spring boot :)

--- collection.java
+++ collection.java
@@ -1,30 +1,30 @@
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.HashSet;
+// import java.util.ArrayList;
+// import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Set;
+// import java.util.HashSet;
 
-public class collection {
+// public class collection {
 
-    public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        System.out.println(list);
+//     public static void main(String[] args) {
+//         ArrayList<Integer> list = new ArrayList<>();
+//         list.add(1);
+//         list.add(2);
+//         list.add(3);
+//         System.out.println(list);
 
-        Map<Integer, String> map = new HashMap<>();
-        map.put(1, "John");
-        map.put(2, "Jane");
-        map.put(3, "Jim");
-        System.out.println(map);
+//         Map<Integer, String> map = new HashMap<>();
+//         map.put(1, "John");
+//         map.put(2, "Jane");
+//         map.put(3, "Jim");
+//         System.out.println(map);
 
-        Set<Integer> set = new HashSet<>();
-        set.add(1);
-        set.add(2);
-        set.add(3);
-        System.out.println(set);
+//         Set<Integer> set = new HashSet<>();
+//         set.add(1);
+//         set.add(2);
+//         set.add(3);
+//         System.out.println(set);
 
 
-    }
-}
+//     }
+// }
--- input.java
+++ input.java
@@ -1,37 +1,37 @@
-// import java.io.BufferedReader;
-// import java.io.InputStreamReader;
-// import java.io.IOException;
+// // import java.io.BufferedReader;
+// // import java.io.InputStreamReader;
+// // import java.io.IOException;
 
 
-//buffered reader
+// //buffered reader
+// // public class input {
+// //     public static void main(String[] args) {
+// //         BufferedReader reader = null;
+// //         try {
+// //             reader = new BufferedReader(new InputStreamReader(System.in));
+// //         System.out.println("Enter your name: ");
+// //         String name = reader.readLine();
+// //         System.out.println("Hello, " + name + "!");
+// //         } catch (IOException e) {
+// //             e.printStackTrace();
+// //         } finally {
+// //             try {
+// //                 reader.close();
+// //             } catch (IOException e) {
+// //                 e.printStackTrace();
+// //             }
+// //         }
+// //     }
+// // }
+
+// import java.util.Scanner;
+// //scanner
 // public class input {
 //     public static void main(String[] args) {
-//         BufferedReader reader = null;
-//         try {
-//             reader = new BufferedReader(new InputStreamReader(System.in));
+//         Scanner scanner = new Scanner(System.in);
 //         System.out.println("Enter your name: ");
-//         String name = reader.readLine();
+//         String name = scanner.nextLine();
 //         System.out.println("Hello, " + name + "!");
-//         } catch (IOException e) {
-//             e.printStackTrace();
-//         } finally {
-//             try {
-//                 reader.close();
-//             } catch (IOException e) {
-//                 e.printStackTrace();
-//             }
-//         }
 //     }
 // }
-
-import java.util.Scanner;
-//scanner
-public class input {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
-    }
-}
 
