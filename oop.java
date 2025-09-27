0687229c383b3eff525504f633f833c760ddd76b
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-09-27 13:10:36 +0700

    commit progress

--- oop.java
+++ oop.java
@@ -173,41 +173,223 @@
 
 //multi inheritance
 
-class Calculator{
+// class Calculator{
 
-    public int add(int a, int b){
-        return a + b;
-    }
-    public int subtract(int a, int b){
-        return a - b;
-    }
-    public int multiply(int a, int b){
-        return a * b;
+//     public int add(int a, int b){
+//         return a + b;
+//     }
+//     public int subtract(int a, int b){
+//         return a - b;
+//     }
+//     public int multiply(int a, int b){
+//         return a * b;
+//     }
+// }
+
+// class AdvancedCalculator extends Calculator{
+
+//     public int divide(int a, int b){
+//         return a / b;
+//     }
+
+// }
+
+// class expercalculator extends AdvancedCalculator{
+//     public int power(int a, int b){
+//         return (int) Math.pow(a, b);
+//     }
+// }
+
+// public class oop {
+//     public static void main(String[] args) {
+
+//         expercalculator benda = new expercalculator();
+//         System.out.println(benda.add(10, 20));
+//         System.out.println(benda.subtract(10, 20));
+//         System.out.println(benda.multiply(10, 20));
+//         System.out.println(benda.divide(10, 20));
+//         System.out.println(benda.power(10, 20));
+//     }
+// }
+
+
+//super
+
+// class student{
+//     private int id;
+//     private String name;
+
+//     public student(int id, String name){
+//         this.id = id;
+//         this.name = name;
+//     }
+
+//     public int getId(){
+//         return id;
+//     }
+
+//     public String getName(){
+//         return name;
+//     }
+
+// }
+
+// class experstudent extends student{
+
+//     public experstudent(){
+//         super(2, "ewewew");
+//     }
+
+//     public experstudent(int id, String name){
+//         super(id, name);
+//     }
+
+//     public String getNamefromchild(){
+//         return getName();
+//     }
+// }
+
+// public class oop {
+//     public static void main(String[] args) {
+//         experstudent benda = new experstudent(1, "John");
+//         System.out.println(benda.getId());
+//         System.out.println(benda.getName());
+//         System.out.println(benda.getNamefromchild());
+
+//         experstudent benda2 = new experstudent();
+//         System.out.println(benda2.getId());
+//         System.out.println(benda2.getName());
+//         System.out.println(benda2.getNamefromchild());
+//     }
+// }   
+
+
+//super and child class constructor call with this() and super()
+
+// class student{
+//     private int id;
+//     private String name;
+
+//     public student(int id, String name){
+//         this.id = id;
+//         this.name = name;
+//         System.out.println("parent constructor");
+//     }
+
+//     public int getId(){
+//         return id;
+//     }
+
+//     public String getName(){
+//         return name;
+//     }
+
+// }
+
+// class experstudent extends student{
+
+//     public experstudent(){
+//         super(2, "ewewew");
+//         System.out.println("child constructor");
+//     }
+
+//     public experstudent(int id, String name){
+//         this();   //---> call public experstudent(){
+//         System.out.println("child constructor with parameters");
+//     }
+
+//     public String getNamefromchild(){
+//         return getName();
+//     }
+// }
+
+// public class oop {
+//     public static void main(String[] args) {
+//         experstudent benda = new experstudent(1, "John");
+//         System.out.println(benda.getId());
+//         System.out.println(benda.getName());
+//         System.out.println(benda.getNamefromchild());
+
+//     }
+// }   
+
+//overide method parent class
+
+// class student {
+//     private int id;
+//     private String name;
+
+//     public student(int id, String name){
+//         this.id = id;
+//         this.name = name;
+//     }
+
+//     public String getidname(){
+//         return id + " " + name;
+//     }
+
+//     public int getId(){
+//         return id;
+//     }
+//     public String getName(){
+//         return name;
+//     }
+// }
+
+// class experstudent extends student{ 
+//     public experstudent(int id, String name){
+//         super(id, name);
+//     }
+
+//     public String getidname(){
+//         return getId() + " " + getName() + " " + super.getidname() + " overide";
+//     }
+
+// }
+
+// public class oop {
+//     public static void main(String[] args) {
+//         experstudent benda = new experstudent(1, "John");
+//         System.out.println(benda.getidname());
+//     }
+// }
+
+
+//polymorphism  -- dynamic method dispatch
+class A{
+
+    public void display(){
+        System.out.println("Class A");
     }
 }
 
-class AdvancedCalculator extends Calculator{
+class B extends A{
 
-    public int divide(int a, int b){
-        return a / b;
+    public void display(){
+        System.out.println("Class B");
     }
 
-}
-
-class expercalculator extends AdvancedCalculator{
-    public int power(int a, int b){
-        return (int) Math.pow(a, b);
+    public void display2(){
+        System.out.println("Class B 2");
     }
 }
 
-public class oop {
+class C extends A{
+    public void display(){
+        System.out.println("Class C");
+    }
+}
+
+class oop{
     public static void main(String[] args) {
+        A obj = new A();
+        obj.display();
+        obj = new B();
+        obj.display();
+        obj = new C();
+        obj.display();
 
-        expercalculator benda = new expercalculator();
-        System.out.println(benda.add(10, 20));
-        System.out.println(benda.subtract(10, 20));
-        System.out.println(benda.multiply(10, 20));
-        System.out.println(benda.divide(10, 20));
-        System.out.println(benda.power(10, 20));
+        // jadi obj itu bergeser addresnya ke object baru di heap --> dynamic method dispatch
     }
 }
+
